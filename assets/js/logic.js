8655b029c703bf2dfc1b567112fdb83fc3d05c03
Obaid Gulzar <obaid3337@googlemail.com>
2024-01-16 03:32:25 +0000

    for loop added

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,3 +2,7 @@ let questionDiv = document.getElementById("questions");
 let questionTitle = document.getElementById("question-title");
 let choices = document.getElementById("choices");
 
+for (let i = 0; i < quizQuestions.length; i++) {
+    questionTitle = quizQuestions[i];
+    console.log(questionTitle);
+};
