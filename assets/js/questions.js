7d4f6af4c1d3856ae4c959236327498926e7e019
Obaid Gulzar <obaid3337@googlemail.com>
2024-01-16 03:15:37 +0000

    questions added into array with each question as an object

---
+++ assets/js/questions.js
@@ -0,0 +1,53 @@
+const quizQuestions = [
+    {
+      question: "What does HTML stand for?",
+      options: ["HyperText Markup Language", "High-Level Textual Markup Language", "Hyperlink and Text Markup Language", "Hypertext Model Language"],
+      correctAnswer: "HyperText Markup Language"
+    },
+    {
+      question: "Which HTML tag is used for creating an ordered list?",
+      options: ["<ul>", "<ol>", "<li>", "<dl>"],
+      correctAnswer: "<ol>"
+    },
+    {
+      question: "How can you apply an inline style in CSS?",
+      options: ["style attribute", "class attribute", "id attribute", "css attribute"],
+      correctAnswer: "style attribute"
+    },
+    {
+      question: "What are the main components of the CSS box model?",
+      options: ["Margin, Border, Padding, Content", "Border, Padding, Image, Content", "Margin, Padding, Background, Content", "Box, Model, Style, Content"],
+      correctAnswer: "Margin, Border, Padding, Content"
+    },
+    {
+      question: "Which keyword is used to declare a variable in JavaScript?",
+      options: ["var", "let", "const", "variable"],
+      correctAnswer: "var"
+    },
+    {
+      question: "How do you define a function in JavaScript?",
+      options: ["function myFunction()", "var myFunction = function()", "() => myFunction()", "define function myFunction()"],
+      correctAnswer: "function myFunction()"
+    },
+    {
+      question: "What is the purpose of the <form> tag in HTML?",
+      options: ["Creating hyperlinks", "Defining a section in a document", "Creating a user input form", "Styling the webpage"],
+      correctAnswer: "Creating a user input form"
+    },
+    {
+      question: "Which CSS selector selects all elements with a specific class?",
+      options: [".class", "#id", "* (asterisk)", "element.class"],
+      correctAnswer: ".class"
+    },
+    {
+      question: "Which loop in JavaScript continues execution as long as the specified condition evaluates to true?",
+      options: ["for loop", "while loop", "do-while loop", "if loop"],
+      correctAnswer: "while loop"
+    },
+    {
+      question: "What is the purpose of the <header> tag in HTML?",
+      options: ["Representing a container for metadata", "Defining a header for a document or a section", "Creating a hyperlink", "Adding a footer to the page"],
+      correctAnswer: "Defining a header for a document or a section"
+    }
+  ];
+  
