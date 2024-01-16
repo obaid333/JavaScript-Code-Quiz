456171b8f12c6f427f1826c54940cf8ffb31fed6
Obaid Gulzar <obaid3337@googlemail.com>
2024-01-16 03:46:40 +0000

    when start buton is pressed innerHTML is emptied so nothing is on screen

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,17 @@
 let questionDiv = document.getElementById("questions");
 let questionTitle = document.getElementById("question-title");
 let choices = document.getElementById("choices");
+let startScreen = document.getElementById("start-screen");
+
+
+document.addEventListener("click", function(){
+    startScreen.innerHTML = "";
+})
+
+
+
+
+
 
 for (let i = 0; i < quizQuestions.length; i++) {
     questionTitle = quizQuestions[i];
